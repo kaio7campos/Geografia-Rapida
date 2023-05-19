aa5d98cf5d32ce322a85de9730a3bbd88adaefc2
Caio de Oliveira Campos <127210789+kaio7campos@users.noreply.github.com>
2023-05-19 17:42:37 -0300

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// 6) Escreva  um  algoritmo  para  ler  as  dimensões  de  um  retângulo
-// (base  e  altura),  calcular  e  escrever  a área do retângulo.
-function area() {
-    let base = parseInt(prompt('Digite a base:'))
-    let altura = parseInt(prompt('Digite a altura'))
-    area = base * altura
-    alert("A area do triangulo é de:"+' '+area)
-}
